26c635abedebc1e13745b0186f7b343d011087af
heija959 <heija959>
2022-03-22 16:46:20 +1300

    Fixed repeats

--- DocSearch/DocSearch.java
+++ DocSearch/DocSearch.java
@@ -1,6 +1,7 @@
 package DocSearch;
 import java.io.*;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class DocSearch {
@@ -10,15 +11,18 @@ public class DocSearch {
         InvertedIndexObject index;
         long duration;
         long startTime;
+        ArrayList<Long> durlist = new ArrayList<Long>();
+
 
         // Load index and info objects
-        for (int j = 0; j <= 1000; j++) {
+        for (int j = 0; j <= 10; j++) {
             startTime = System.nanoTime();
             index = grabInvertedIndex(Path.of("index"));
 
             duration = ((System.nanoTime() - startTime) / 1000000);  //divide by 1000000 to get milliseconds.
             //System.out.println("Load: " + duration + "ms");
-            System.out.println(duration);
+            //System.out.println(duration);
+            durlist.add(duration);
 
             Map<String, int[]> map = index.getMap();
             int[] results = (map.get("Zealand"));
@@ -30,6 +34,7 @@ public class DocSearch {
             //System.out.println("Search: " + duration + "ms");
             index = null;
         }
+        System.out.println(durlist);
     }
 
     public static InvertedIndexObject grabInvertedIndex(Path source) throws IOException, ClassNotFoundException {
