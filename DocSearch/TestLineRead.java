a18aeeaa798d169b73aed097c35c841be0350e4b
heija959 <heija959>
2022-04-06 17:31:01 +1200

    Line read ver 1

--- DocSearch/TestLineRead.java
+++ DocSearch/TestLineRead.java
@@ -1,7 +1,11 @@
 package DocSearch;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
@@ -13,12 +17,19 @@ public class TestLineRead {
         long endTime = System.nanoTime();
         long duration = ((endTime - startTime) / 1000000);
         System.out.println(in + " " + duration + "ms");
+        startTime = System.nanoTime();
     }
 
-    public static void main(String[] args) {
-        int n = 400000; // The line number
+    public static Object readDisk(Path source) throws IOException, ClassNotFoundException {
+        ObjectInputStream o = new ObjectInputStream(new BufferedInputStream(new FileInputStream(source.toFile())));
+        return o.readObject();
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        int n = 4500; // The line number
         String line;
-        try (Stream<String> lines = Files.lines(Paths.get("wsj.xml"))) {
+        readDisk(Path.of("dictionary"));
+        try (Stream<String> lines = Files.lines(Paths.get("check"))) {
             line = lines.skip(n).findFirst().get();
             System.out.println(line);
         } catch (IOException e) {
